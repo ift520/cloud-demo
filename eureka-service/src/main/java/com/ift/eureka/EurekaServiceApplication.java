2276f28059a8c4c6f156c2fc796ff52f0ff9e12f
ift520 <liu.ift@gmail.com>
2020-03-30 21:25:11 +0800

    修改至Version2

--- eureka-service/src/main/java/com/ift/eureka/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/ift/eureka/EurekaServiceApplication.java
@@ -3,13 +3,29 @@ package com.ift.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class EurekaServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return "Version1.0";
+    }
+
+    @GetMapping("/date")
+    public String date() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
     }
 
 }
